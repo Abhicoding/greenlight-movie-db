b6142812f29fa96bbb5cb3e4831f7b77e66927fb
Abhishek Shingane <abhishekshingane@Abhisheks-MBP.Dlink>
2024-01-02 10:31:24 +0530

    start http web server

---
+++ cmd/api/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+// Declare a string containing the application version number. Later in the book we'll
+// generate this automatically at build time, but for now we'll just store the version
+// number as a hard-coded global constant.
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for our application.
+// For now, the only configuration settings will be the network port that we want the
+// server to listen on, and the name of the current operating environment for the
+// application (development, staging, production, etc.). We will read in these
+// configuration settings from command-line flags when the application starts.
+type config struct {
+	port int
+	env  string
+}
+
+// Define an application struct to hold the dependencies for our HTTP handlers, helpers,
+// and middleware. At the moment this only contains a copy of the config struct and a
+// logger, but it will grow to include a lot more as our build progresses.
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	//Declare instance of the config struct
+	var cfg config
+
+	fmt.Println("Hello World!")
+	// Read the value of the port and env command-line flags into the config struct. We
+	// default to using the port number 4000 and the environment "development" if no
+	// corresponding flags are provided.
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment development|staging|production")
+
+	// Initialize a new logger which writes messages to the standard out stream,
+	// prefixed with the current date and time.
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module greenlight.abhicoding
+
+go 1.16
